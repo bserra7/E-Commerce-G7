cb2d2bbaf89d1d7acf0eac627119f41e1b222c1d
JonathanR6 <jonathangh2030@gmail.com>
2022-04-07 12:59:56 -0300

    fix component

--- client/src/components/DashboardUser.jsx
+++ client/src/components/DashboardUser.jsx
@@ -1,44 +1,50 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import { NavLink, useHistory } from 'react-router-dom';
 import useUser from './Login/hooks/useUser';
 import UserAccount from './UserAccount';
 import { FormattedMessage } from 'react-intl';
 
-
-export function DashboardUser(){
+export function DashboardUser() {
 
     const history = useHistory()
-
     const { isLogged } = useUser();
 
     useEffect(() => {
-        if(!isLogged) history.push('/')
-    },[])
+        if (!localStorage.getItem('jwt') && !sessionStorage.getItem('jwt')) history.push("/")
+        axios.post('/authenticate', { token: localStorage.getItem('jwt') })
+            .then()
+            .catch(res => {
+                axios.post('/authenticate', { token: sessionStorage.getItem('jwt') })
+                    .then()
+                    .catch(res => history.push("/"))
+            })
+    }, [isLogged])
 
     return (
         <>
-        <nav className='dashboardUser'>
-            <div className='container'>
-                <li className='dashboardUser__item'>
-                    <NavLink to="/user/account/profile"
-                        activeClassName='active'
-                        className="dashboardUser__link"><FormattedMessage id="app.profile" defaultMessage="Profile"/></NavLink>
-                </li>
-                <li className='dashboardUser__item'>
-                    <NavLink to="/user/account/orders"
-                        activeClassName='active' className="dashboardUser__link"> <FormattedMessage id="app.view-order" defaultMessage="View Orders"/></NavLink>
-                </li>
-                <li className='dashboardUser__item'>
-                    <NavLink to='/user/account/reset-password'
-                        activeClassName='active' className="dashboardUser__link"><FormattedMessage id="app.change-password" defaultMessage="Change Password"/></NavLink>
-                </li>
-                <li className='dashboardUser__item'>
-                    <NavLink to='/user/account/wishlist'
-                        activeClassName='active' className="dashboardUser__link"><FormattedMessage id="app.wishlist" defaultMessage="My Wishlist"/></NavLink>
-                </li>
-            </div>
-        </nav>
-        {history.location.pathname === "/user/account" && <UserAccount />}
+            <nav className='dashboardUser'>
+                <div className='container'>
+                    <li className='dashboardUser__item'>
+                        <NavLink to="/user/account/profile"
+                            activeClassName='active'
+                            className="dashboardUser__link"><FormattedMessage id="app.profile" defaultMessage="Profile" /></NavLink>
+                    </li>
+                    <li className='dashboardUser__item'>
+                        <NavLink to="/user/account/orders"
+                            activeClassName='active' className="dashboardUser__link"> <FormattedMessage id="app.view-order" defaultMessage="View Orders" /></NavLink>
+                    </li>
+                    <li className='dashboardUser__item'>
+                        <NavLink to='/user/account/reset-password'
+                            activeClassName='active' className="dashboardUser__link"><FormattedMessage id="app.change-password" defaultMessage="Change Password" /></NavLink>
+                    </li>
+                    <li className='dashboardUser__item'>
+                        <NavLink to='/user/account/wishlist'
+                            activeClassName='active' className="dashboardUser__link"><FormattedMessage id="app.wishlist" defaultMessage="My Wishlist" /></NavLink>
+                    </li>
+                </div>
+            </nav>
+            {history.location.pathname === "/user/account" && <UserAccount />}
         </>
     )
 }
--- client/src/components/TwoFaActivate.jsx
+++ client/src/components/TwoFaActivate.jsx
@@ -5,60 +5,60 @@ import swal from 'sweetalert';
 import { getUserDetail, userLogin, verifyTwoFA } from "../redux/actions";
 import { FormattedMessage, useIntl } from 'react-intl'
 
-export default function TwoFaActivate(){
+export default function TwoFaActivate() {
     const dispatch = useDispatch();
     const intl = useIntl();
     const userLogged = useSelector(state => state.user);
     const userDetail = useSelector(state => state.userDetail)
 
-    useEffect(()=> {
-        dispatch(getUserDetail(userLogged?.id))
-    },[])
+    useEffect(() => {
+        if (userLogged) dispatch(getUserDetail(userLogged?.id))
+    }, [])
 
-    useEffect(()=> {
-        dispatch(userLogin(userDetail))
-    },[userDetail])
+    useEffect(() => {
+        if (Object.keys(userDetail).length) return dispatch(userLogin(userDetail))
+    }, [userDetail])
 
     const handle2FA = async (boolean) => {
         try {
-            const response = await axios.put('/twofa', {action: 'set', userId: userLogged?.id, two_fa: boolean})
-            if(response.status === 200 && boolean === true) {
+            const response = await axios.put('/twofa', { action: 'set', userId: userLogged?.id, two_fa: boolean })
+            if (response.status === 200 && boolean === true) {
                 swal({
-                  title: intl.formatMessage({ id: "message-active" }),
-                  text: intl.formatMessage({ id: "message-active-success" }),
-                  icon: 'success',
-                  timer: 3000,
-                  button: null
+                    title: intl.formatMessage({ id: "message-active" }),
+                    text: intl.formatMessage({ id: "message-active-success" }),
+                    icon: 'success',
+                    timer: 3000,
+                    button: null
                 })
-            } else if(response.status === 200 && boolean === false) {
+            } else if (response.status === 200 && boolean === false) {
                 swal({
                     title: intl.formatMessage({ id: "message-deactive" }),
                     text: intl.formatMessage({ id: "message-deactive-success" }),
                     icon: 'warning',
                     timer: 3000,
                     button: null
-                  })
+                })
             }
             dispatch(verifyTwoFA());
             dispatch(getUserDetail(userLogged?.id));
         } catch (error) {
             swal({
                 title: intl.formatMessage({ id: "message-error" }),
-                text:  intl.formatMessage({ id: "message-error-check" }),
+                text: intl.formatMessage({ id: "message-error-check" }),
                 icon: 'error',
                 timer: 3000,
                 button: null
-              })
+            })
         }
 
     }
-    
-    return(
+
+    return (
         <>
-        {!userDetail?.is_two_fa ? 
-            <button className="userAccount__button" onClick={e => handle2FA(true)}><FormattedMessage id="app.active" defaultMessage="Activate 2FA"/></button>
-        :
-            <button className="userAccount__button" onClick={e => handle2FA(false)}><FormattedMessage id="app.deactive" defaultMessage="Deactivate 2FA"/></button>}
+            {!userDetail?.is_two_fa ?
+                <button className="userAccount__button" onClick={e => handle2FA(true)}><FormattedMessage id="app.active" defaultMessage="Activate 2FA" /></button>
+                :
+                <button className="userAccount__button" onClick={e => handle2FA(false)}><FormattedMessage id="app.deactive" defaultMessage="Deactivate 2FA" /></button>}
         </>
     )
 }
